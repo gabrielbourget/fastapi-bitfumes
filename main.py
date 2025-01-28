6c49c2edf300707c80c776f20168cbfa486b76d9
Gabriel B <gaboxa@syntronic.com>
2025-01-28 10:37:38 -0500

    tutorial work

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 """main app file"""
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+class Blog(BaseModel):
+  """blog schema"""
+  title: str
+  body: str
+  published: Optional[bool]
 
 app = FastAPI()
 
@@ -12,6 +20,8 @@ async def index():
 @app.get("/blogs")
 async def blogs(limit = 10, published: bool = True, sort: Optional[str] = None):
   """unpublished blog endpoint"""
+  if sort == "Ascending":
+    return { "data": "sort parameter included" }
   if published:
     return { "data": f"{limit} published blog posts" }
 
@@ -32,8 +42,16 @@ async def get_blog_comments(blog_id: int, limit = 10):
   """get blog comments endpoint"""
   return { "comments": ["comment 1", "comment 2", "comment 3"], "id": blog_id, "limit": limit }
 
+@app.post("/blogs")
+async def create_blog(blog: Blog):
+  """create blog endpoint"""
+  return { "data": blog }
 
 @app.get("/about")
 async def about():
   """about endpoint"""
   return { "data": "about endpoint" }
+
+
+# if __name__ == "__main__":
+#   uvicorn.run(app, host = "127.0.0.1", port = 9000)
